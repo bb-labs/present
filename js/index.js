0fb4b5d807b7cfcb16211800867ffebd2896b676
trumanpurnell <truman.purnell@gmail.com>
2021-03-17 16:39:44 -0600

    codegen fails for jsx

--- js/index.js
+++ js/index.js
@@ -1,27 +1,66 @@
 import fs from 'fs'
 import path from 'path'
+import esprima from 'esprima'
+import escodegen from 'escodegen'
 
-const excludeExts = new Set(['.json', '.md'])
-const excludeDirs = new Set(['node_modules', '.git', 'assets', 'dist'])
-const excludeFile = new Set(['bundle.js', 'bundle.js.map', '.gitignore'])
+export default class Presentation {
+  constructor({ repo, excludeDirs, excludeExts, excludeFile }) {
+    this.plan = {}
 
-export default function walk(tree) {
-  const files = []
+    this.excludeDirs = new Set(excludeDirs)
+    this.excludeExts = new Set(excludeExts)
+    this.excludeFile = new Set(excludeFile)
 
-  if (fs.lstatSync(tree).isFile()) {
-    files.push(tree)
+    this.walk(repo).forEach(path => {
+      const source = fs.readFileSync(path, 'utf8')
+
+      this.plan[path] = { imports: {}, exports: {}, classes: {}, functions: {} }
+
+      try { esprima.parseModule(source, { range: true }, this.createPlan.bind(this, path)) } catch (e) { }
+      try { esprima.parseScript(source, { range: true }, this.createPlan.bind(this, path)) } catch (e) { }
+
+    }, this)
+  }
+
+  createPlan(path, node) {
+    switch (node.type) {
+      case esprima.Syntax.ImportDeclaration:
+        return this.plan[path].imports[node.source.value] = escodegen.generate(node)
+
+      case esprima.Syntax.MethodDefinition:
+        return this.plan[path].functions[node.key.name] = escodegen.generate(node)
+
+      case esprima.Syntax.ClassDeclaration:
+        return this.plan[path].classes[node.id.name] = escodegen.generate(node)
+
+      case esprima.Syntax.ExportDefaultDeclaration:
+        return this.plan[path].exports.default = escodegen.generate(node)
+    }
+  }
+
+  walk(tree) {
+    const files = []
+
+    if (fs.lstatSync(tree).isFile()) {
+      files.push(tree)
+
+      return files
+    }
+
+    for (const file of fs.readdirSync(tree)) {
+      if (!this.excludeDirs.has(file)
+        && !this.excludeExts.has((file.match(/\..+/g) || []).pop())
+        && !this.excludeFile.has(file))
+        files.push(...this.walk(path.join(tree, file)))
+    }
 
     return files
   }
-
-  for (const file of fs.readdirSync(tree)) {
-    if (!excludeDirs.has(file)
-      && !excludeExts.has((file.match(/\..+/g) || []).pop())
-      && !excludeFile.has(file))
-      files.push(...walk(path.join(tree, file)))
-  }
-
-  return files
 }
 
-console.log(walk('/Users/trumanpurnell/Workspace/labs/chess'))
+new Presentation({
+  repo: '/Users/trumanpurnell/Workspace/labs/chess/view/components/App.jsx',
+  excludeExts: ['.json', '.md'],
+  excludeDirs: ['node_modules', '.git', 'assets', 'dist'],
+  excludeFile: ['bundle.js', 'bundle.js.map', '.gitignore'],
+})
